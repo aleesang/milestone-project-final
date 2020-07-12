44c5af9c15b1c3a24b820147d48a295acb247674
aleesang <aleesang20@gmail.com>
2020-07-12 19:12:52 +0930

    updated product detail with sizes

--- products/migrations/0002_auto_20200712_0731.py
+++ products/migrations/0002_auto_20200712_0731.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RemoveField(
             model_name='product',
+            name='rating',
         ),
         migrations.AddField(
             model_name='product',
