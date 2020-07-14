fe5e1d0ddbc39c0189d9506c669a47cdf913d188
aleesang <aleesang20@gmail.com>
2020-07-14 22:32:55 +0930

    added toasts

--- bag/views.py
+++ bag/views.py
@@ -3,8 +3,8 @@ from django.contrib import messages
 
 from products.models import Product
 
-
 # Create your views here.
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
@@ -12,8 +12,8 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
- 
-    product = Product.objects.get(ok=item_id)
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -21,14 +21,16 @@ def add_to_bag(request, item_id):
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
+    
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+            bag[item_id] += quantity
     else:
-        bag[item_id] = quantity
-        messages.success(request, f'Added {product.name} successfully to your bag')
+            bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
@@ -56,6 +58,8 @@ def adjust_bag(request, item_id):
     return redirect(reverse('view_bag'))
 
 
+
+    
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
