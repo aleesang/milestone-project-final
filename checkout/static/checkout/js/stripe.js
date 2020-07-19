c94a07e2a4716901d24cb5609e6981e7cf208736
aleesang <aleesang20@gmail.com>
2020-07-19 16:23:59 +0930

    installed stripe

---
+++ checkout/static/checkout/js/stripe.js
@@ -0,0 +1,29 @@
+// Set up Stripe.js and Elements to use in checkout form
+var setupElements = function(data) {
+    stripe = Stripe(data.publishableKey);
+    var elements = stripe.elements();
+    var style = {
+      base: {
+        color: "#32325d",
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#aab7c4"
+        }
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a"
+      }
+    };
+  
+    var card = elements.create("card", { style: style });
+    card.mount("#card-element");
+  
+    return {
+      stripe: stripe,
+      card: card,
+      clientSecret: data.clientSecret
+    };
+  };
