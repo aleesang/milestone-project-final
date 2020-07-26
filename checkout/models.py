f8f27b5b4c9259180803c349882bf3f450d26b8b
aleesang <aleesang20@gmail.com>
2020-07-26 21:08:34 +0930

    made migrations

--- checkout/models.py
+++ checkout/models.py
@@ -28,37 +28,6 @@ class Order(models.Model):
             total += order_item.get_final_price()
         return total
     
-    def _generate_order_number(self):
-        """
-        Generate a random, unique order number using UUID
-        """
-        return uuid.uuid4().hex.upper()
-
-    def update_total(self):
-        """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
-        """
-        self.order_total = self.lineitems.aggregate(Sum('item_total'))['item_total__sum'] or 0
-        if self.order_total < settings.FREE_DELIVERY:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-        else:
-            self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
-        self.save()
-
-    def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the order number
-        if it hasn't been set already.
-        """
-        if not self.order_number:
-            self.order_number = self._generate_order_number()
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.order_number
-    
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, blank=True, null=True, default=None, on_delete=models.CASCADE, related_name='order_items')
     product = models.ForeignKey(Product, blank=True, null=True, default=None, on_delete=models.CASCADE)
@@ -73,6 +42,4 @@ class OrderItem(models.Model):
         self.item_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-    def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
     
