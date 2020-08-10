0ddd68c9d43a3c8ba04837b939bbf90246e54312
aleesang <aleesang20@gmail.com>
2020-08-10 13:16:45 +0930

    amended email settings

--- milestone_project_final/settings.py
+++ milestone_project_final/settings.py
@@ -208,6 +208,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_USER = os.environ.get('EMAIL_USER')
-    EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
