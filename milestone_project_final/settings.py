50304703b8b06f35836f5c674e47de75a9458c81
aleesang <aleesang20@gmail.com>
2020-08-18 07:27:24 +0930

    updated script tags

--- milestone_project_final/settings.py
+++ milestone_project_final/settings.py
@@ -119,12 +119,11 @@ WSGI_APPLICATION = 'milestone_project_final.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
 if "DATABASE_URL" in os.environ:
-    DATABASES = {'default':
-                 dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+    DATABASES = {'default': dj_database_url.parse(
+                 os.environ.get('DATABASE_URL'))}
 else:
-    print("Database URL not found. Using SQLite instead")
+    print('Database URL not found, using SQLite instead')
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
