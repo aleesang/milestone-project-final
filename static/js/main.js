8580dd4ab3a6e8a1bf0a2422f246b73a4bcc1b1b
aleesang <aleesang20@gmail.com>
2020-08-18 13:14:26 +0930

    added parallex effect

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,16 @@
 $(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.fadein').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1000);
-                    
-            }
-            
-        }); 
-    
-    });
+
+/* parallax */
+var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+
+/* toasts */
+$('.toast').toast('show');
 
 });
