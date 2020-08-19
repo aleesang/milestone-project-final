05b0a6043933484d7aa242d4613d2f28d24daa6d
aleesang <aleesang20@gmail.com>
2020-08-19 14:33:40 +0930

    changed urls.py customorder

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,5 +4,5 @@ from .views import checkout, checkout_success
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('order_success', views.order_success, name='order_success'),
+    path('checkout_success', views.checkout_success, name='checkout_success'),
 ]
