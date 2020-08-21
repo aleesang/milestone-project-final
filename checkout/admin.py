b23dbb2e63a8346d78243c33b7fff1bf46b2a5d5
aleesang <aleesang20@gmail.com>
2020-08-21 14:17:57 +0930

    changed admin.py in profiles

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,36 +4,47 @@ from .models import Order, OrderItem
 
 class OrderItemAdminInline(admin.TabularInline):
     model = OrderItem
+    readonly_fields = ('item_total',)
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderItemAdminInline,)
     
-    readonly_fields = ('order_number', 
-                'date',
-                'full_name',
-                'order_total', 
-                'delivery_cost',
-                'final_total',)
+    readonly_fields = (
+            'order_number', 
+            'date',
+            'full_name',
+            'order_total', 
+            'delivery_cost',
+            'final_total',
+            'original_bag',
+            'stripe_pid')
     
-    fields = ('order_number',
-              'date',
-              'full_name', 
-              'email', 
-              'phone_number',
-              'street_address', 
-              'address2',
-              'country', 
-              'town_or_city', 
-              'postcode', 
-              'delivery_cost',
-              'order_total', 
-              'final_total',) 
+    fields = (
+            'order_number',
+            'user_profile',
+            'date',
+            'full_name', 
+            'email', 
+            'phone_number',
+            'street_address', 
+            'address2',
+            'country', 
+            'town_or_city', 
+            'postcode', 
+            'delivery_cost',
+            'order_total', 
+            'final_total',
+            'original_bag',
+            'stripe_pid') 
 
-list_display = ('order_number',
-                'date', 
-                'full_name',
-                'order_total', 
-                'delivery_cost',
-                'final_total',)
+    list_display = (
+            'order_number',
+            'date', 
+            'full_name',
+            'order_total', 
+            'delivery_cost',
+            'final_total',)
+
+    ordering = ('-date',)
 
 admin.site.register(Order, OrderAdmin)
