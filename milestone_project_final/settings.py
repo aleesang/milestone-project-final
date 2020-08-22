2740662de9ec42e214d52f1aa85a9e75688f494e
aleesang <aleesang20@gmail.com>
2020-08-22 21:10:58 +0930

    set debug false

--- milestone_project_final/settings.py
+++ milestone_project_final/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tech-and-co.herokuapp.com', '127.0.0.1', 'localhost', '*']
 
